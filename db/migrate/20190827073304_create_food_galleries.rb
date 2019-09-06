dd2020e9d5e805284cd361642ef44ba36530875a
smondal229 <smondal229@gmail.com>
2019-09-06 10:01:09 +0530

    foreign cascading added

--- db/migrate/20190827073304_create_food_galleries.rb
+++ db/migrate/20190827073304_create_food_galleries.rb
@@ -1,9 +1,8 @@
 class CreateFoodGalleries < ActiveRecord::Migration[5.2]
   def change
     create_table :food_galleries do |t|
-      t.references :food_store, foreign_key: true
+      t.references :food_store, foreign_key: { on_delete: :cascade }
       t.text :description
-      t.timestamps
     end
   end
 end
