b67bf748d7d86f0169a398c97188c643ea05d360
Suvodip Mondal <smondal229@gmail.com>
2019-09-13 12:20:38 +0530

    Update 20190824033005_create_food_stores.rb

--- db/migrate/20190824033005_create_food_stores.rb
+++ db/migrate/20190824033005_create_food_stores.rb
@@ -2,6 +2,7 @@ class CreateFoodStores < ActiveRecord::Migration[5.2]
   def change
     create_table :food_stores do |t|
       t.string :name
+      t.timestamps
     end
     
     add_index :food_stores, :name, unique: true
