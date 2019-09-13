5f20f0777fdee11e2c16282cb21f3bd7b49f5826
Suvodip Mondal <smondal229@gmail.com>
2019-09-13 13:15:42 +0530

    Update 20190830044359_create_messages.rb

--- db/migrate/20190830044359_create_messages.rb
+++ db/migrate/20190830044359_create_messages.rb
@@ -4,6 +4,7 @@ class CreateMessages < ActiveRecord::Migration[5.2]
       t.references :sendable, polymorphic: true
       t.references :recipientable, polymorphic: true
       t.text :body, null: false
+      
       t.timestamps
     end
   end
