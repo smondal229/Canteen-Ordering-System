270a50f3016e396c9e50a049158915ae469361fe
Suvodip Mondal <smondal229@gmail.com>
2019-09-13 13:05:30 +0530

    Update 20190827073304_create_food_galleries.rb

--- db/migrate/20190827073304_create_food_galleries.rb
+++ db/migrate/20190827073304_create_food_galleries.rb
@@ -3,6 +3,8 @@ class CreateFoodGalleries < ActiveRecord::Migration[5.2]
     create_table :food_galleries do |t|
       t.references :food_store, foreign_key: { on_delete: :cascade }
       t.text :description
+      
+      t.timestamps
     end
   end
 end
