6da8c9faa728d2a5bb4d70623f664e4457645782
Suvodip Mondal <smondal229@gmail.com>
2019-09-13 12:22:51 +0530

    Update 20190824033020_create_categories.rb

--- db/migrate/20190824033020_create_categories.rb
+++ db/migrate/20190824033020_create_categories.rb
@@ -1,7 +1,8 @@
 class CreateCategories < ActiveRecord::Migration[5.2]
   def change
     create_table :categories do |t|
-      t.string :name
+      t.string :name, null: false
+      t.timestamps
     end
 
     add_index :categories, :name, unique: true
