0fbcc40c8d8efdd7f94fc8f47d59d7cdff6d614a
Suvodip Mondal <smondal229@gmail.com>
2019-09-13 13:09:08 +0530

    Update 20190829100025_create_cart_items.rb

--- db/migrate/20190829100025_create_cart_items.rb
+++ db/migrate/20190829100025_create_cart_items.rb
@@ -1,10 +1,12 @@
 class CreateCartItems < ActiveRecord::Migration[5.2]
   def change
     create_table :cart_items do |t|
-      t.integer :quantity, default: 1
+      t.integer :quantity, null: false, default: 1
       t.references :food, foreign_key: { on_delete: :cascade }
       t.text :description
       t.belongs_to :cart, foreign_key: { on_delete: :cascade }
+      
+      t.timestamps
     end
   end
 end
