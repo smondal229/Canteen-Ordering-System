4677d7dcfba9fbfdd6795ccdbe1767969b44063d
Suvodip Mondal <smondal229@gmail.com>
2019-09-13 12:19:44 +0530

    Update 20190822093431_create_companies.rb

--- db/migrate/20190822093431_create_companies.rb
+++ db/migrate/20190822093431_create_companies.rb
@@ -2,6 +2,7 @@ class CreateCompanies < ActiveRecord::Migration[5.2]
   def change
     create_table :companies do |t|
       t.string :name, null: false
+      t.timestamps
     end
     
     add_index :companies, :name, unique: true
