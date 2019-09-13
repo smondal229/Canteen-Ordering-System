ccb9d887aa7d9232d6e9201c8e7b806d09dac109
Suvodip Mondal <smondal229@gmail.com>
2019-09-13 13:12:11 +0530

    Update 20190903070958_create_notifications.rb

--- db/migrate/20190903070958_create_notifications.rb
+++ db/migrate/20190903070958_create_notifications.rb
@@ -3,6 +3,7 @@ class CreateNotifications < ActiveRecord::Migration[5.2]
     create_table :notifications do |t|
       t.text :content
       t.references :notifiable, polymorphic: true
+      
       t.timestamps
     end
   end
