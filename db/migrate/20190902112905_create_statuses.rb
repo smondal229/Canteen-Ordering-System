a829dc952e222a3860c013f299ce8e0fcc1c3093
Suvodip Mondal <smondal229@gmail.com>
2019-09-13 13:10:55 +0530

    Update 20190902112905_create_statuses.rb

--- db/migrate/20190902112905_create_statuses.rb
+++ db/migrate/20190902112905_create_statuses.rb
@@ -2,6 +2,8 @@ class CreateStatuses < ActiveRecord::Migration[5.2]
   def change
     create_table :statuses do |t|
       t.string :name, null: false
+      
+      t.timestamps
     end
     add_index :statuses, :name, unique: true
   end
